9307994d2c0895b62d6c99a4919a25665bbce31c
Victor Martinez <victorjbamartinez@gmail.com>
2025-02-11 10:04:34 -0300

    add Cloth Simulation

---
+++ Cloth Simulation/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include "Application.h"
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+    Application app;
+
+    app.Setup(1200, 320, 10);
+
+    while(app.IsRunning()) 
+    {
+        app.Input();
+        app.Update();
+        app.Render();
+    }
+
+    app.Destroy();
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Rotating Cube: 3D Graphics With Draw Pixel - Rotating Cube SDL2 C++ Project / The Builder
+
+Lorenz Attractor: Coding Challenge #12: The Lorenz Attractor in Processing
+
+Cloth Simulation:
+    https://pikuma.com/blog/verlet-integration-2d-cloth-physics-simulation
+    https://github.com/marianpekar/cloth-simulation-2d/tree/master/source
